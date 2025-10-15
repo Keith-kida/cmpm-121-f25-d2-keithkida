129e0e0ef2ae738252b6301f131d7863cccfb913
Keith Kida <62222088+Keith-kida@users.noreply.github.com>
2025-10-15 07:36:19 +0000

    Step 1 complete: I added a title onto main.ts as well as added a canvas to both the style.css and main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,14 @@ import "./style.css";
 
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  
 `;
+
+const title = document.createElement("h1");
+title.textContent = "Let's Paint";
+document.body.append(title);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+document.body.append(canvas);
