bc61e753abda3c85e8f2683852473df4fb5269b0
Keith Kida <62222088+Keith-kida@users.noreply.github.com>
2025-10-16 04:56:55 +0000

    Step 3 completed: saves the  mouse positions into an array of arrays of points.

--- src/main.ts
+++ src/main.ts
@@ -9,30 +9,61 @@ canvas.width = 256;
 canvas.height = 256;
 document.body.append(canvas);
 
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d")!;
 
+type Point = { x: number; y: number };
+type Line = Point[];
+
+const lines: Line[] = [];
+const redoLines: Line[] = [];
+let currentLine: Line | null = null;
 const cursor = { active: false, x: 0, y: 0 };
 
 canvas.addEventListener("mousedown", (e) => {
   cursor.active = true;
   cursor.x = e.offsetX;
   cursor.y = e.offsetY;
+
+  currentLine = [];
+  lines.push(currentLine);
+  redoLines.splice(0, redoLines.length);
+  currentLine.push({ x: cursor.x, y: cursor.y });
+
+  redraw();
 });
 
 canvas.addEventListener("mousemove", (e) => {
-  if (cursor.active && ctx) {
-    ctx.beginPath();
-    ctx.moveTo(cursor.x, cursor.y);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
+  if (cursor.active && ctx && currentLine) {
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
+
+    currentLine.push({ x: cursor.x, y: cursor.y });
+
+    redraw();
   }
 });
 
 canvas.addEventListener("mouseup", () => {
   cursor.active = false;
+  currentLine = null;
+
+  redraw();
 });
+
+function redraw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (const line of lines) {
+    if (line.length > 1) {
+      ctx.beginPath();
+      const { x, y } = line[0]!;
+      ctx.moveTo(x, y);
+      for (const { x, y } of line) {
+        ctx.lineTo(x, y);
+      }
+      ctx.stroke();
+    }
+  }
+}
 
 const clearButton = document.createElement("button");
 clearButton.innerHTML = "clear";
@@ -40,6 +71,7 @@ document.body.append(clearButton);
 
 clearButton.addEventListener("click", () => {
   if (ctx) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    lines.splice(0, lines.length);
+    redraw();
   }
 });
